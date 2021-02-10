01ed0a39ae467a446f18a2e40fcb9f87e534bcbe
Mandy Wade <lightninggirl413@hotmail.com>
2021-02-10 18:42:10 -0500

    Got the Team Name to Print out Winner

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 function main() {
+  // Change Team 1's Name
   if (document.querySelector('section.team1 fieldset input placeholder')) {
     document.querySelector(
       'section.team1 fieldset input placeholder'
@@ -6,10 +7,11 @@ function main() {
   }
 
   function changeTeam1Name(event) {
-    const newTeam1Name = document.querySelector('section.team1 h2')
+    var newTeam1Name = document.querySelector('section.team1 h2')
     newTeam1Name.textContent = event.target.value
   }
 
+  // Change Team 2's Name
   if (document.querySelector('section.team2 fieldset input placeholder')) {
     document.querySelector(
       'section.team2 fieldset input placeholder'
@@ -21,27 +23,28 @@ function main() {
     newTeam2Name.textContent = event.target.value
   }
 
+  // Team 1 Add Function
   const team1AddButton = document.querySelector('section.team1 fieldset i.add')
   const team1SubtractButton = document.querySelector(
     'section.team1 fieldset i.subtract'
   )
   let team1Score = 0
   function handClickT1AddButton(event) {
-    const thingClickedOn = event.target
-
-    if (team1Score === 21) {
-      window.alert(`Yay! Team 1 got to 21 Points First! Team 1 Won!!!`)
-      thingClickedOn.classList.add('taken')
-      return
-    }
-    if (thingClickedOn.classList.contains('taken')) {
-      console.log('Nopes....')
-      return
-    }
     team1Score++
-    const team1ScoreHeader = document.querySelector('section.team1 h3')
-    team1ScoreHeader.textContent = `${team1Score}`
+    if (team1Score <= 21) {
+      const team1ScoreHeader = document.querySelector('section.team1 h3')
+      team1ScoreHeader.textContent = `${team1Score}`
+    }
+    if (team1Score === 21) {
+      const newName = document.querySelector('section.team1 h2')
+      const team1Name = newName.textContent
+      return window.alert(
+        `Yay! ${team1Name} got to 21 Points First! ${team1Name} Won!!!`
+      )
+    }
   }
+
+  // Team 1 Subtract Function
   function handClickT1SubtractButton(event) {
     if (team1Score === 0) {
       console.log('Nopes...')
@@ -52,28 +55,28 @@ function main() {
     team1ScoreHeader.textContent = `${team1Score}`
   }
 
+  // Team 2 Add Function
   const team2AddButton = document.querySelector('section.team2 fieldset i.add')
   const team2SubtractButton = document.querySelector(
     'section.team2 fieldset i.subtract'
   )
   let team2Score = 0
   function handClickT2AddButton(event) {
-    const thingClickedOn = event.target
-
-    if (team2Score === 21) {
-      window.alert('Yay! Team 2 got to 21 Points First! Team 2 Won!!! ')
-      thingClickedOn.classList.add('taken')
-      return
-    }
-    if (thingClickedOn.classList.contains('taken')) {
-      console.log('Nopes....')
-      return
-    }
-
     team2Score++
-    const team2ScoreHeader = document.querySelector('section.team2 h3')
-    team2ScoreHeader.textContent = `${team2Score}`
+    if (team2Score <= 21) {
+      const team2ScoreHeader = document.querySelector('section.team2 h3')
+      team2ScoreHeader.textContent = `${team2Score}`
+    }
+    if (team2Score === 21) {
+      const newName = document.querySelector('section.team2 h2')
+      const team2Name = newName.textContent
+      return window.alert(
+        `Yay! ${team2Name} got to 21 Points First! ${team2Name} Won!!!`
+      )
+    }
   }
+
+  // Team 2 Subtract Function
   function handClickT2SubtractButton(event) {
     if (team2Score === 0) {
       console.log('Nopes...')
@@ -83,6 +86,8 @@ function main() {
     const team2ScoreHeader = document.querySelector('section.team2 h3')
     team2ScoreHeader.textContent = `${team2Score}`
   }
+
+  // Reset Button
   const resetButton = document.querySelector('section button')
   function resetButtonClick(event) {
     const team1ScoreHeader = document.querySelector('section.team1 h3')
@@ -92,17 +97,20 @@ function main() {
     team2ScoreHeader.textContent = 0
     team2Score = 0
   }
-
-  team1AddButton.addEventListener('click', handClickT1AddButton)
-  team2AddButton.addEventListener('click', handClickT2AddButton)
-  team1SubtractButton.addEventListener('click', handClickT1SubtractButton)
-  team2SubtractButton.addEventListener('click', handClickT2SubtractButton)
   document
     .querySelector('section.team1 input')
     .addEventListener('input', changeTeam1Name)
+
   document
     .querySelector('section.team2 input')
     .addEventListener('input', changeTeam2Name)
+
+  team1AddButton.addEventListener('click', handClickT1AddButton)
+  team2AddButton.addEventListener('click', handClickT2AddButton)
+
+  team1SubtractButton.addEventListener('click', handClickT1SubtractButton)
+  team2SubtractButton.addEventListener('click', handClickT2SubtractButton)
+
   resetButton.addEventListener('click', resetButtonClick)
 }
 
